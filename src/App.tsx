98b9c8746206a543b8ba3334ad00eb38ec6a422e
Sam Matenaer <smatenaer@esri.com>
2024-01-27 12:44:31 -0600

    add better codeinput display and video controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
+// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { CodeInput } from "./components/CodeInput";
 import { VideoPlayer } from "./components/VideoPlayer";
 
 function App() {
+  const [page, setPage] = useState<"title" | "game">("game");
+
+  if (page === "title") {
+    return (
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-6xl">Laff Tracker</h1>
+        <button
+          onClick={() => setPage("game")}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Start
+        </button>
+      </div>
+    );
+  }
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center gap-4">
       <h1>Soundboard</h1>
-      <div>
-        <VideoPlayer />
-      </div>
-      <div>
-        {/* make a grid of four squares */}
-        <CodeInput />
-      </div>
+      <VideoPlayer />
+      {/* make a grid of four squares */}
+      <CodeInput />
     </div>
   );
 }
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import YouTubePlayer from "youtube-player";
+import { YouTubePlayer as YouTubePlayerType } from "youtube-player/dist/types";
 
 enum VideoState {
   Unstarted = -1,
@@ -17,19 +18,16 @@ export function VideoPlayer() {
     "playing" | "paused" | "stopped"
   >("stopped");
 
+  const [player, setPlayer] = useState<YouTubePlayerType>();
+
   useEffect(() => {
     const player = YouTubePlayer(videoRef.current!);
 
+    setPlayer(player);
+
     // 'loadVideoById' is queued until the player is ready to receive API calls.
-    player.loadVideoById("M7lc1UVf-VE");
+    player.loadVideoById("WcYG-5b7448");
 
-    // 'playVideo' is queue until the player is ready to received API calls and after 'loadVideoById' has been called.
-    player.playVideo();
-
-    // 'stopVideo' is queued after 'playVideo'.
-    player.stopVideo().then(() => {
-      // Every function returns a promise that is resolved after the target function has been executed.
-    });
     player.on("stateChange", (e) => {
       const state = e.data as VideoState;
 
@@ -46,22 +44,57 @@ export function VideoPlayer() {
       }
     });
 
-    // get the current duration every 0.1 seconds
-    // const interval = setInterval(async () => {
-
-    //   const currentTime = await player.getCurrentTime();
-
-    // }, 1000);
-
-    // return () => {
-    //   clearInterval(interval);
-    // };
+    return () => {
+      player.destroy();
+    };
   }, []);
 
   return (
-    <div>
-      <div>{videoState}</div>
-      <div ref={videoRef} className=" w-96" />{" "}
+    <div className="flex flex-col gap-4 items-center">
+      <div ref={videoRef} className="w-96 h-auto aspect-video rounded-xl" />
+
+      {/* pause/play button */}
+      <button
+        onClick={() => {
+          if (videoState === "playing") {
+            player?.pauseVideo();
+          } else {
+            player?.playVideo();
+          }
+        }}
+        className="p-4 border-2 rounded-lg shadow-hard"
+      >
+        {videoState === "playing" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <rect x="6" y="4" width="4" height="16"></rect>
+            <rect x="14" y="4" width="4" height="16"></rect>
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <polygon points="5 3 19 12 5 21 5 3"></polygon>
+          </svg>
+        )}
+      </button>
     </div>
   );
 }
