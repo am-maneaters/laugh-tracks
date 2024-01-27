4aab6c20a434e84a65fc5c233305d51e525ad10e
Joel Schuman <foibles@gmail.com>
2024-01-27 11:26:05 -0600

    add constants and audio

---
+++ src/constants.ts
@@ -0,0 +1,89 @@
+export const fileManifest = [
+  {
+    id: 0,
+    filename: "audio/awww.mp3",
+    title: "Aww",
+  },
+  {
+    id: 1,
+    filename: "audio/disappointed_shock.mp3",
+    title: "Shock",
+  },
+  {
+    id: 2,
+    filename: "audio/gasp.mp3",
+    title: "Gasp",
+  },
+  {
+    id: 3,
+    filename: "audio/good_joke.mp3",
+    title: "Chuckles",
+  },
+  {
+    id: 4,
+    filename: "audio/laugh_but_you_shouldnt.mp3",
+    title: "Guilty Laugh",
+  },
+  {
+    id: 5,
+    filename: "audio/ooaah.mp3",
+    title: "Groan",
+  },
+  {
+    id: 6,
+    filename: "audio/solid_laugh.mp3",
+    title: "Solid Laugh",
+  },
+  {
+    id: 7,
+    filename: "audio/applause.mp3",
+    title: "Applause",
+  },
+  {
+    id: 8,
+    filename: "audio/cheering.mp3",
+    title: "Cheering",
+  },
+  {
+    id: 9,
+    filename: "audio/polite_applause.mp3",
+    title: "Golf Clap",
+  },
+  {
+    id: 10,
+    filename: "audio/baby_laugh.mp3",
+    title: "Laughing Baby",
+  },
+  {
+    id: 11,
+    filename: "audio/clearing_throat.mp3",
+    title: "Man Clearing Throat",
+  },
+];
+
+export const scoreReference_TEST = [
+  {
+    elapsed: 3000,
+    soundPlayedId: 0,
+  },
+  {
+    elapsed: 5000,
+    soundPlayedId: 1,
+  },
+  {
+    elapsed: 10000,
+    soundPlayedId: 2,
+  },
+  {
+    elapsed: 13000,
+    soundPlayedId: 3,
+  },
+  {
+    elapsed: 14000,
+    soundPlayedId: 4,
+  },
+  {
+    elapsed: 18000,
+    soundPlayedId: 5,
+  },
+];
