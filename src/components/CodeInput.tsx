bab73b6dcde46e8bb7010d2cdbf6eb40a803891d
Sam Matenaer <smatenaer@esri.com>
2024-01-27 11:22:23 -0600

    update videoplayer and codeinput

--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -1,30 +1,183 @@
+import clsx from "clsx";
+import { useState } from "react";
+
 function CodeInputButton({
   label,
   onClick,
   color,
 }: {
   label: string;
-  onClick: () => void;
+  onClick: (label: string) => void;
   color?: string;
 }) {
   return (
     <button
-      className="aspect-square text-6xl rounded border-black border cursor-pointer font-bold p-4 hover:border-blue-400"
-      onClick={onClick}
-      style={{backgroundColor: color, color: 'black'}}
+      className="w-24 h-24 text-6xl rounded border-black border cursor-pointer font-bold p-4 hover:border-blue-400 shadow-hard active:translate-x-fill-shadow-x active:shadow-none active:translate-y-fill-shadow-y"
+      onClick={() => onClick(label)}
+      style={{ backgroundColor: color, color: "black" }}
     >
       {label}
     </button>
   );
 }
 
+const CodeMatches = [
+  {
+    code: "∆∆∆∆",
+    name: "Laugh",
+    soundEffect: "",
+  },
+  {
+    code: "∆∆◊◊",
+    name: "Aww",
+    soundEffect: "",
+  },
+  {
+    code: "∆◊∆◊",
+    name: "Applause",
+    soundEffect: "",
+  },
+  {
+    code: "∆◊◊∆",
+    name: "Boo",
+    soundEffect: "",
+  },
+  {
+    code: "◊∆∆∆",
+    name: "Gasp",
+    soundEffect: "",
+  },
+  {
+    code: "◊∆◊∆",
+    name: "Giggle",
+    soundEffect: "",
+  },
+  {
+    code: "◊◊∆∆",
+    name: "Guffaw",
+    soundEffect: "",
+  },
+  {
+    code: "◊◊◊◊",
+    name: "Sigh",
+    soundEffect: "",
+  },
+  {
+    code: "∞∞∞∞",
+    name: "Scream",
+    soundEffect: "",
+  },
+  {
+    code: "∞∞∞◊",
+    name: "Scream",
+    soundEffect: "",
+  },
+  {
+    code: "∞∞◊∞",
+    name: "Scream",
+    soundEffect: "",
+  },
+  {
+    code: "∞◊∞∞",
+    name: "Scream",
+    soundEffect: "",
+  },
+  {
+    code: "∞∞◊◊",
+    name: "Scream",
+    soundEffect: "",
+  },
+  {
+    code: "∞◊∞◊",
+    name: "Scream",
+    soundEffect: "",
+  },
+  {
+    code: "∞◊◊∞",
+    name: "Scream",
+    soundEffect: "",
+  },
+  {
+    code: "∞◊◊◊",
+    name: "Scream",
+    soundEffect: "",
+  },
+  {
+    code: "◊∞∞∞",
+    name: "Scream",
+    soundEffect: "",
+  },
+  {
+    code: "◊∞∞◊",
+    name: "Scream",
+    soundEffect: "",
+  },
+  {
+    code: "◊∞◊∞",
+    name: "Scream",
+    soundEffect: "",
+  },
+];
+
+function validateCode(code: string) {
+  return CodeMatches.find((codeMatch) => codeMatch.code === code);
+}
+
 export function CodeInput() {
+  const [code, setCode] = useState<string>("");
+  const [animateOut, setAnimateOut] = useState<"fail" | "success">();
+
+  const handleCodeInput = (label: string) => {
+    const newCode = code + label;
+
+    setCode(newCode);
+
+    if (newCode.length === 4) {
+      const codeMatch = validateCode(newCode);
+      if (codeMatch) {
+        console.log(codeMatch.name);
+      }
+      setAnimateOut(codeMatch ? "success" : "fail");
+      setTimeout(() => {
+        setAnimateOut(undefined);
+        setCode("");
+      }, 1000);
+      return;
+    }
+  };
+
   return (
-    <div className="flex gap-4">
-      <CodeInputButton label="◊" color="red" onClick={() => {}} />
-      <CodeInputButton label="∆" color="blue" onClick={() => {}} />
-      <CodeInputButton label="∞" color="yellow" onClick={() => {}} />
-      <CodeInputButton label="∂" color="green" onClick={() => {}} />
+    <div className="flex flex-col items-center">
+      <div>
+        <input
+          className={clsx(
+            "border border-black rounded p-2 text-6xl",
+            animateOut === "fail" && "animate-blinking-red",
+            animateOut === "success" && "animate-blinking-green"
+          )}
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+          readOnly
+        />
+      </div>
+      <div className="grid gap-4 grid-cols-2 p-4">
+        <CodeInputButton label="◊" color="red" onClick={handleCodeInput} />
+        <CodeInputButton label="∆" color="blue" onClick={handleCodeInput} />
+        <CodeInputButton label="∞" color="yellow" onClick={handleCodeInput} />
+        <CodeInputButton label="∂" color="green" onClick={handleCodeInput} />
+      </div>
+
+      {/* Display the codes for the player */}
+      <div className="grid gap-4 grid-cols-2 p-4">
+        {CodeMatches.map((codeMatch) => (
+          <div className="flex flex-col items-center">
+            <div className="border border-black rounded p-2 text-6xl">
+              {codeMatch.code}
+            </div>
+            <div className="text-2xl">{codeMatch.name}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,8 +1,21 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import YouTubePlayer from "youtube-player";
+
+enum VideoState {
+  Unstarted = -1,
+  Ended = 0,
+  Playing = 1,
+  Paused = 2,
+  Buffering = 3,
+  Cued = 5,
+}
 
 export function VideoPlayer() {
   const videoRef = useRef<HTMLIFrameElement>(null);
+
+  const [videoState, setVideoState] = useState<
+    "playing" | "paused" | "stopped"
+  >("stopped");
 
   useEffect(() => {
     const player = YouTubePlayer(videoRef.current!);
@@ -17,18 +30,38 @@ export function VideoPlayer() {
     player.stopVideo().then(() => {
       // Every function returns a promise that is resolved after the target function has been executed.
     });
-    player.on("stateChange", (e) => console.log(e));
+    player.on("stateChange", (e) => {
+      const state = e.data as VideoState;
+
+      switch (state) {
+        case VideoState.Playing:
+          setVideoState("playing");
+          break;
+        case VideoState.Paused:
+          setVideoState("paused");
+          break;
+        case VideoState.Ended:
+          setVideoState("stopped");
+          break;
+      }
+    });
 
     // get the current duration every 0.1 seconds
     // const interval = setInterval(async () => {
-      
+
     //   const currentTime = await player.getCurrentTime();
-     
+
     // }, 1000);
 
     // return () => {
     //   clearInterval(interval);
     // };
   }, []);
-  return <div ref={videoRef} className=" w-96" />;
+
+  return (
+    <div>
+      <div>{videoState}</div>
+      <div ref={videoRef} className=" w-96" />{" "}
+    </div>
+  );
 }
