30dd48bfe322bdb6129e0fc00426dbe44444ae7c
Carlo Supina <cdsupina@gmail.com>
2024-01-28 12:05:16 -0600

    add new buttons and tv frame

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -6,12 +6,21 @@ export function MainMenu({ onGameStart }: MainMenuProps) {
   return (
     <div className="flex flex-col items-center gap-4">
       <img src="src/assets/images/logos/laff_tracker_logo.png"></img>
+      <img src="src/assets/images/background/tv_frame_small.png" className="h-96"></img>
       <button
         onClick={() => onGameStart()}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-black hover:bg-white text-white hover:text-black font-handwritten font-bold text-3xl py-4 px-24 rounded-full"
       >
         Start
       </button>
+      <a 
+        href="https://globalgamejam.org/games/2024/laff-tracker-5" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="bg-black hover:bg-white text-white hover:text-black font-handwritten font-bold text-3xl py-4 px-24 rounded-full"
+      >
+        GGJ
+      </a>
     </div>
   );
 }
