f00b34cb5e36efd80e6162ce89694845b4f84c1c
Carlo Supina <cdsupina@gmail.com>
2024-01-28 11:35:56 -0600

    add logo for laff tracker to main menu

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -5,7 +5,7 @@ interface MainMenuProps {
 export function MainMenu({ onGameStart }: MainMenuProps) {
   return (
     <div className="flex flex-col items-center gap-4">
-      <h1 className="text-8xl font-black text-black font-handwritten">Laff Tracker</h1>
+      <img src="src/assets/images/logos/laff_tracker_logo.png"></img>
       <button
         onClick={() => onGameStart()}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
